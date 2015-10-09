c4ee010eb2e76c72db66ace7296637e08dea5a48
12021999 <matolisou@gmail.com>
2015-10-09 11:14:09 -0300

    LIA
    -Progrraming almost  ready
    -new scenes
    -new texts

---
+++ Assets/Scripts/Programming/CompareText.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Text;
+using UnityEngine.UI;
+
+public class CompareText : MonoBehaviour {
+
+    public GameObject CodeLine;
+    void CompareTexts()
+    {
+        if (this.GetComponent<Text>().text == CodeLine.GetComponent<Text>().text)
+        {
+            print("consegui");
+        }
+    }
+	void Update () 
+    {
+        CompareTexts();
+	}
+}
